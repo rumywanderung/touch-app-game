a5277ef07845017e5698f432282467a17c90d6c8
Roumiana Bourkova <48831445+rumywanderung@users.noreply.github.com>
2020-01-23 16:56:58 +0100

    working on random spawn

---
+++ app quick/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int NumGhosts;
+    int i;
+    float Timer;
+
+    void Start()
+    {
+        NumGhosts = 10;
+        i = 0;
+        Timer = 0;
+    }
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        Debug.Log(i);
+        #region Create Ghosts
+        if (i == 16)
+        {
+            return;
+        }
+
+        else
+        {
+            Debug.Log("else");
+
+            for (i = 0; i <= 2; i++)
+            {
+                //if (i % 3 == 0) // multiple of 3
+                if (Timer % 2 == 0)
+                {
+                    Vector3 randomized = new Vector3(Random.Range(0F, 10F), Random.Range(0F, 15F), 0F);
+                    GameObject Ghost = Resources.Load("Ghost") as GameObject;
+                    Instantiate(Ghost, randomized, Quaternion.identity);
+                    Timer = 0;
+                }
+            }
+        }
+       
+
+        #endregion
+    }
+}
---
+++ app quick/Assets/Scripts/Points.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Android;
+//using UnityEngine.iOS;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+
+    private float width;
+    private float height;
+    Renderer cubeRend;
+
+    void Start()
+    {
+        width = (float)Screen.width;
+        height = (float)Screen.height;
+        cubeRend = this.GetComponent<Renderer>();
+    }
+
+    private void OnGUI()
+    {
+        /*
+        GUI.skin.label.fontSize = (int)(Screen.width / 2f);
+        GUI.Label(new Rect(20, 20, width, height * 0.25f));
+        */
+    }
+    
+    void Update()
+    {
+        #region MOBILE
+        //MOBILE (to be continued)
+
+        /* if (Input.touchCount > 0)
+         {
+             Touch touch = Input.GetTouch(1);
+             Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+             Ray ray = Camera.main.ScreenPointToRay(touch.position);
+             RaycastHit hit;
+             //Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 100F);
+
+             if(Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "cube1")
+             {
+                 if (hit.collider != null)
+                 {
+                     if (touch.phase == TouchPhase.Began && cubeRend.gameObject.tag == "cube1")
+                     {
+                         this.cubeRend.material.SetColor("_Color", Color.blue);
+                     }
+                 }
+             }
+
+         }*/
+        #endregion
+        
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "cube1")
+                   {
+                     if (cubeRend.gameObject.tag == "cube1")
+                     {
+                         Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 100F);
+                         //this.cubeRend.material.SetColor("_Color", Color.red);
+                         Destroy(this.gameObject);
+                         Debug.Log("mouse click");
+                     }
+                }
+            }
+            
+        }
+    }
+}
