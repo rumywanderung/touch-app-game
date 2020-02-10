79aee9812a7cf024901cac5ec9a5b81b6e5d9003
Roumiana Bourkova <48831445+rumywanderung@users.noreply.github.com>
2020-02-10 12:05:25 +0100

    PH - score condition done

---
+++ app pizzahut2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float Timer;
+    int i;
+    public Text score; //score text
+    public GameObject pointsmanager2 = null;
+    public PointsManager pointsmanager = null;
+    //[HideInInspector]
+    public bool loaded = false;
+    public bool canvaLoaded = false;
+    public GameObject Objects = null;
+    public GameObject Objects2 = null;
+    public Dictionary<int, string> ObjectDict = new Dictionary<int, string>();
+    Scene currentScene;
+    public GameObject canva = null;
+    public GameObject buttonEnd;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void Start()
+    {
+        buttonEnd = null;
+        Timer = 0;
+        i = 0;
+        ObjectDict.Add(0, "Cheese3");
+        ObjectDict.Add(1, "tomato");
+        ObjectDict.Add(5, "champignon");
+        ObjectDict.Add(3, "poivron");
+        ObjectDict.Add(4, "oignon");
+        ObjectDict.Add(2, "PIZZAHUTLOGO");
+        ObjectDict.Add(6, "Bazil");
+
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    void LoadEndScene()
+    {
+        if (loaded == true)
+        {
+            return;
+        }
+       
+        else if (loaded == false)
+        {
+            loaded = true;
+            DontDestroyOnLoad(canva.gameObject);
+            DontDestroyOnLoad(score.gameObject);
+
+            SceneManager.LoadScene("End");
+        }
+        
+    }
+
+    void Update()
+    {
+
+        if (SceneManager.GetSceneByName("End").isLoaded)
+            {
+            if (score != null)
+            {
+                if (pointsmanager.myPoints < 150)
+                {   score.text = "FINAL SCORE: " + pointsmanager.myPoints.ToString() + "/150";
+                    buttonEnd = Instantiate(Resources.Load("Button End"), new Vector3(19, 35, 0), Quaternion.identity) as GameObject;
+                    Canvas canvaEnd = FindObjectOfType<Canvas>();
+                    buttonEnd.transform.SetParent(canvaEnd.transform, false);
+                }
+                else if (pointsmanager.myPoints >= 150)
+                {
+                    score.text = "FINAL SCORE: " + pointsmanager.myPoints.ToString();
+                    buttonEnd = Instantiate(Resources.Load("Button Next"), new Vector3(19, 35, 0), Quaternion.identity) as GameObject;
+                    Canvas canvaEnd = FindObjectOfType<Canvas>();
+                    buttonEnd.transform.SetParent(canvaEnd.transform, false);
+                }
+            }
+                pointsmanager = null;
+                pointsmanager2 = null;
+            }
+
+            if (SceneManager.GetSceneByName("Game").isLoaded)
+            {
+                if (pointsmanager2 == null && pointsmanager == null)
+            {
+                pointsmanager2 = Instantiate((Resources.Load("PointsManager") as GameObject), new Vector3(0, 0, 0), Quaternion.identity);
+                pointsmanager = pointsmanager2.GetComponent<PointsManager>();
+            }
+                
+
+            if (canvaLoaded == false && canva == null)
+                {
+                    canva = Instantiate((Resources.Load("CanvasScore") as GameObject), new Vector3(0, 0, 0), Quaternion.identity);
+                    score = canva.GetComponentInChildren<Text>();
+                    canvaLoaded = true;
+                }
+
+                i++;
+                Timer += Time.deltaTime;
+               
+                #region Create Objects
+
+                if (Timer >= 40F) //500 | 10
+                {
+                    score.transform.position = new Vector3(((Screen.width / 2) - 47), (Screen.height / 2) + 750, 0F);
+                    LoadEndScene();
+
+                    return;
+                }
+
+                else if (Timer < 40F && i % 12 == 0)
+                {
+                    Vector3 randomized = new Vector3(Random.Range(0F, 12F), Random.Range(5F, 15F), 0F);
+                    int index = Random.Range(0, 7);
+                    Objects = Resources.Load(ObjectDict[index]) as GameObject;
+                    Objects2 = Instantiate(Objects, randomized, Quaternion.identity);
+                    Destroy(Objects2, 2F);
+                }
+            }
+
+            #endregion
+
+
+            if (score != null)
+            {
+                score.text = "Score: " + pointsmanager.myPoints.ToString();
+            }
+
+            
+
+       
+        
+    }
+        
+}
+
---
+++ app pizzahut2/Assets/Scripts/PointsManager.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Android;
+//using UnityEngine.iOS;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour
+{
+    private float width;
+    private float height;
+    Renderer cubeRend;
+    Ray ray;
+    GameManager manager;
+    [HideInInspector]
+    public float myPoints;
+    public GameObject visualFX;
+    public GameObject explosionFX;
+
+    void Start()
+    {
+        width = (float)Screen.width;
+        height = (float)Screen.height;
+        cubeRend = this.GetComponent<Renderer>();
+        manager = FindObjectOfType<GameManager>();
+        DontDestroyOnLoad(this.gameObject);
+        myPoints = 0;
+    }
+
+    void Update()
+    {
+
+        #region MOBILE
+        //MOBILE (to be continued)
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity) && hit.collider.gameObject.tag == "clickableObject")
+            {
+                myPoints += 1;
+                visualFX = Resources.Load("PPUn") as GameObject;
+                Vector3 pos = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y + 2, 0F);
+                Instantiate(visualFX, pos, Quaternion.identity);
+                Destroy(hit.transform.gameObject);
+                
+                explosionFX = Resources.Load("Explosion") as GameObject;
+                Vector3 pos2 = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y, 0F);
+                Instantiate(explosionFX, pos2, Quaternion.identity);
+
+                visualFX = null;
+                explosionFX = null;
+            }
+            else if (Physics.Raycast(ray, out hit, Mathf.Infinity) && hit.collider.gameObject.tag == "clickableObject3")
+            {
+                myPoints += 5;
+                visualFX = Resources.Load("PPCinco") as GameObject;
+                Vector3 pos = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y + 2, 0F);
+                Instantiate(visualFX, pos, Quaternion.identity);
+                Destroy(hit.transform.gameObject);
+                
+                explosionFX = Resources.Load("Explosion") as GameObject;
+                Vector3 pos2 = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y, 0F);
+                Instantiate(explosionFX, pos2, Quaternion.identity);
+
+                visualFX = null;
+                explosionFX = null;
+            }
+
+            else if (Physics.Raycast(ray, out hit, Mathf.Infinity) && hit.collider.gameObject.tag == "clickableObject2")
+            {
+                myPoints -= 1;
+                visualFX = Resources.Load("MinusONE2") as GameObject;
+                Vector3 pos = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y + 2, 0F);
+                Instantiate(visualFX, pos, Quaternion.identity);
+                Destroy(hit.transform.gameObject);
+                
+                visualFX = null;
+                explosionFX = null;
+            }
+
+            visualFX = null;
+            explosionFX = null;
+        }
+
+       
+
+        #endregion
+        
+       if (Input.GetMouseButtonDown(0) == true)
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.tag == "clickableObject")
+                {
+                    myPoints += 1;
+                    visualFX = Resources.Load("PPUn") as GameObject;
+                    Vector3 pos = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y + 2, 0F);
+                    Instantiate(visualFX, pos, Quaternion.identity);
+                    Destroy(hit.transform.gameObject);
+                    
+                    explosionFX = Resources.Load("Explosion") as GameObject;
+                    Vector3 pos2 = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y, 0F);
+                    Instantiate(explosionFX, pos2, Quaternion.identity);
+
+                    visualFX = null;
+                    explosionFX = null;
+                }
+                else if (hit.collider.gameObject.tag == "clickableObject3")
+                {
+                    myPoints += 5;
+                    visualFX = Resources.Load("PPCinco") as GameObject;
+                    Vector3 pos = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y + 2, 0F);
+                    Instantiate(visualFX, pos, Quaternion.identity);
+                    Destroy(hit.transform.gameObject);
+                   
+                    explosionFX = Resources.Load("Explosion") as GameObject;
+                    Vector3 pos2 = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y, 0F);
+                    Instantiate(explosionFX, pos2, Quaternion.identity);
+
+                    visualFX = null;
+                    explosionFX = null;
+                }
+
+                else if (Physics.Raycast(ray, out hit, Mathf.Infinity) && hit.collider.gameObject.tag == "clickableObject2")
+                {
+                    myPoints -= 1;
+                    visualFX = Resources.Load("MinusONE2") as GameObject;
+                    Vector3 pos = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y + 2, 0F);
+                    Instantiate(visualFX, pos, Quaternion.identity);
+                    Destroy(hit.transform.gameObject);
+
+                    //explosionFX = Resources.Load("Explosion") as GameObject;
+                    //Vector3 pos2 = new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y, 0F);
+                    //Instantiate(explosionFX, pos2, Quaternion.identity);
+
+                    visualFX = null;
+                    explosionFX = null;
+                }
+            }
+        }
+    }
+}
+
+
